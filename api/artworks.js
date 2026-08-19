e1022afd783d5d9749b2f9cc71a3ebb9d29022a7
kucibok <kucibok221@gmail.com>
2026-08-19 15:43:45 +0000

    fix: remove ETag and Last-Modified headers to prevent 304 responses

--- api/artworks.js
+++ api/artworks.js
@@ -9,9 +9,11 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
-  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate, max-age=0')
   res.setHeader('Pragma', 'no-cache')
   res.setHeader('Expires', '0')
+  res.removeHeader('ETag')
+  res.removeHeader('Last-Modified')
 
   if (req.method === 'OPTIONS') {
     res.status(200).end()
